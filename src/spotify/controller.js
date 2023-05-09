84a9e6db80b167cc2f7934892a9c9aeab728ed15
Ray Merrell <RayMerrell@outlook.com>
2023-05-09 16:14:38 +0100

    spotify login route and controller

--- src/spotify/controller.js
+++ src/spotify/controller.js
@@ -1,24 +1,24 @@
-
 const jwt = require("jsonwebtoken");
 const SpotifyWebAPI = require("spotify-web-api-node");
 
 const login = async (req, res) => {
-    try {
-      const spotifyAPI = new SpotifyWebAPI({
-        clientId: process.env.SPOTIFY_CLIENT_ID,
-        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-        redirectUri: process.env.SPOTIFY_REDIRECT_URI,
-      });
-      const data = await SpotifyWebAPI.authorizationCodeGrant(req.body.code);
-      res.json({
-        accessToken: data.body.access_token,
-        refreshToken: data.body.refresh_token,
-        expiresIn: data.body.expires_in,
-      });
-    } catch (e) {
-      res.sendStatus(400);
-    }
-  };
-  module.exports = {
-    login
-  };
+  try {
+    console.log("spotify login attempted");
+    const spotifyAPI = new SpotifyWebAPI({
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      redirectUri: process.env.SPOTIFY_REDIRECT_URI,
+    });
+    const data = await SpotifyWebAPI.authorizationCodeGrant(req.body.code);
+    res.json({
+      accessToken: data.body.access_token,
+      refreshToken: data.body.refresh_token,
+      expiresIn: data.body.expires_in,
+    });
+  } catch (e) {
+    res.sendStatus(400);
+  }
+};
+module.exports = {
+  login,
+};
